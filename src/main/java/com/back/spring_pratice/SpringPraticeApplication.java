d07eddd69fd9fadfa93c706a4fed04317b2a98d3
bokang03 <bogang12345@gmail.com>
2026-02-24 22:55:01 +0900

    init

---
+++ src/main/java/com/back/spring_pratice/SpringPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.back.spring_pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPraticeApplication.class, args);
+	}
+
+}
